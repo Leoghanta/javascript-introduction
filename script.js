a02e662689c045d60416df8013bdf4d22fb5ad7f
Jimi Wilson <62606625+Leoghanta@users.noreply.github.com>
2024-06-23 01:22:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* JavaScript is a different language to HTML and CSS
+ * and comments are also different.  This is a multi line comment
+ * encased in star-slash and closed with slash-star.
+ */
+
+// This is a single line comment. slash-slash can be added anywhere and will coment until the end of line.
+
+
+// The console.log is a function that writes to the browser's console. 
+console.log("Hello, World!")
+
+// Run this web page and use the developer tools to view the console and find our message.
